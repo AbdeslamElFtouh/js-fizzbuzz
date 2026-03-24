1d3e68863093e743059355b5b1e243d99f630b9a
Abdeslam El Ftouh <abdeslamelftouh@gmail.com>
2026-03-24 18:22:30 +0100

    ADD usato alert per renderlo interattivo per l'utente

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-for (i = 1; i < 101; i++) {
+/* for (i = 1; i < 101; i++) {
     if (i % (3*5) === 0) { 
         console.log('FizzBuzz');
     }else if (i % 3 === 0) {
@@ -7,6 +7,27 @@ for (i = 1; i < 101; i++) {
         console.log('Buzz');
     }else
         console.log(i)
+} */
+
+
+alert('Regole del gioco:\nInserisci un numero compreso tra 1 e 100 che sia multiplo di 3, di 5 o di entrambi per ricevere dei feedback: \n  ogni "Fizz" corrisponde ad un multiplo di 3;\n  ogni "Buzz" corrisponde ad un multiplo di 5;\n  ogni "FizzBuzz" corrisponde ad un multiplo sia di 3 che di 5.\nOra prova e divertiti!')
+
+const num_inserito = prompt('Ciao! Prova ad inserire un numero compreso tra 1 e 100:');
+const n_inserito = parseInt (num_inserito)
+
+
+if (isNaN(n_inserito)) {
+    alert('Mi dispiace, ma sono ammesse soltanto le cifre.');
+} else if (n_inserito < 1 || n_inserito > 100) {
+    alert('I numeri inseriti devono essere compresi tra 1 e 100.'); 
+} else if (n_inserito % (3 * 5) === 0) {
+    alert('FizzBuzz!');
+} else if (n_inserito % 3 === 0) {
+    alert('Fizz!');
+} else if (n_inserito % 5 === 0) {
+    alert('Buzz!');
+} else {
+    alert('Ops... Riprovaci!');
 }
 
 
@@ -50,32 +71,3 @@ for (i = 1; i < 101; i++) {
 
 
 
-
-
-/* const n_inserito = prompt('Ciao! Inserisci un numero compreso tra 1 e 100.');
-
-if (n_inserito === ''){
-    alert('Dovresti inserire qualcosa...');
-
-}else if (n_inserito == isNaN){
-    alert('Sono ammessi soltanto i numeri');
-
-}else if (n_inserito < 1 && n_inserito > 100){
-    alert('Valgono solo i numeri compresi tra 1 e 100');
-
-}else{
-    for(n_inserito; n_inserito > 101; n_inserito++) {
-        if (n_inserito % 3 === 0) {
-            alert('Fizz')
-
-        }if (n_inserito % 5 === 0) {
-            alert('Buzz')
-
-        }if (n_inserito % 3 === 0 && n_inserito % 5 === 0) {
-            alert('FizzBuzz')
-
-        }else {
-            alert('Ops... questo non è un multiplo')
-        }
-    }
-} */
